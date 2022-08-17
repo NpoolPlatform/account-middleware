6518fdf32d3e26d931bec827888dd169c179b59b
Zhao KK <kikakkz@hotmail.com>
2022-08-17 10:48:47 +0000

    Support get accounts

--- api/deposit/query.go
+++ api/deposit/query.go
@@ -3,11 +3,18 @@ package deposit
 
 import (
 	"context"
-	"fmt"
 
 	npool "github.com/NpoolPlatform/message/npool/account/mw/v1/deposit"
+
+	deposit1 "github.com/NpoolPlatform/account-middleware/pkg/deposit"
 )
 
 func (s *Server) GetAccounts(ctx context.Context, in *npool.GetAccountsRequest) (*npool.GetAccountsResponse, error) {
-	return nil, fmt.Errorf("NOT IMPLEMENTED")
+	infos, err := deposit1.GetAccounts(ctx, in.GetConds())
+	if err != nil {
+		return nil, err
+	}
+	return &npool.GetAccountsResponse{
+		Infos: infos,
+	}, nil
 }
