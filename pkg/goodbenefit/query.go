4d6468557cef021af19b59719efabf4b733b6505
Zhao KK <kikakkz@hotmail.com>
2022-10-19 10:21:41 +0000

    Support to create good benefit

--- pkg/goodbenefit/query.go
+++ pkg/goodbenefit/query.go
@@ -5,11 +5,14 @@ import (
 
 	constant "github.com/NpoolPlatform/account-middleware/pkg/message/const"
 	commontracer "github.com/NpoolPlatform/account-middleware/pkg/tracer"
+
 	"go.opentelemetry.io/otel"
 	scodes "go.opentelemetry.io/otel/codes"
 
 	curl "github.com/NpoolPlatform/account-manager/pkg/crud/goodbenefit"
 	"github.com/NpoolPlatform/account-manager/pkg/db/ent/goodbenefit"
+
+	accountmgrpb "github.com/NpoolPlatform/message/npool/account/mgr/v1/account"
 	mgrpb "github.com/NpoolPlatform/message/npool/account/mgr/v1/goodbenefit"
 
 	"entgo.io/ent/dialect/sql"
@@ -46,11 +49,14 @@ func GetAccount(ctx context.Context, id string) (info *npool.Account, err error)
 			Where(
 				goodbenefit.ID(uuid.MustParse(id)),
 			)
-		return join(stm).Scan(ctx, infos)
+		return join(stm).
+			Scan(ctx, &infos)
 	})
 	if err != nil {
 		return nil, err
 	}
+
+	infos = expand(infos)
 
 	return infos[0], nil
 }
@@ -89,6 +95,8 @@ func GetAccounts(ctx context.Context, conds *npool.Conds, offset, limit int32) (
 		return nil, err
 	}
 
+	infos = expand(infos)
+
 	return infos, nil
 }
 
@@ -113,7 +121,13 @@ func join(stm *ent.GoodBenefitQuery) *ent.GoodBenefitSelect {
 					sql.As(t1.C(account.FieldLocked), "locked"),
 					sql.As(t1.C(account.FieldLockedBy), "locked_by"),
 					sql.As(t1.C(account.FieldBlocked), "blocked"),
-					sql.As(t1.C(account.FieldUsedFor), "used_for"),
 				)
 		})
+}
+
+func expand(infos []*npool.Account) []*npool.Account {
+	for _, info := range infos {
+		info.LockedBy = accountmgrpb.LockedBy(accountmgrpb.LockedBy_value[info.LockedByStr])
+	}
+	return infos
 }
