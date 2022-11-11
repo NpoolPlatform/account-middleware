e8736e6c03c88bcadbecc790a95f63a95f4b8bdb
Zhao KK <kikakkz@hotmail.com>
2022-11-11 18:03:48 +0000

    Add get accounts

--- api/account/query.go
+++ api/account/query.go
@@ -1,5 +1,5 @@
 //nolint:nolintlint,dupl
-package deposit
+package account
 
 import (
 	"context"
@@ -8,47 +8,16 @@ import (
 	constant "github.com/NpoolPlatform/account-middleware/pkg/message/const"
 	commontracer "github.com/NpoolPlatform/account-middleware/pkg/tracer"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-	"github.com/google/uuid"
+
 	"go.opentelemetry.io/otel"
 	scodes "go.opentelemetry.io/otel/codes"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	npool "github.com/NpoolPlatform/message/npool/account/mw/v1/deposit"
-
-	deposit1 "github.com/NpoolPlatform/account-middleware/pkg/deposit"
+	accmgrcli "github.com/NpoolPlatform/account-manager/pkg/client/account"
+	accmgrpb "github.com/NpoolPlatform/message/npool/account/mgr/v1/account"
+	npool "github.com/NpoolPlatform/message/npool/account/mw/v1/account"
 )
-
-func (s *Server) GetAccount(ctx context.Context, in *npool.GetAccountRequest) (*npool.GetAccountResponse, error) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "GetAccount")
-	defer span.End()
-
-	defer func() {
-		if err != nil {
-			span.SetStatus(scodes.Error, err.Error())
-			span.RecordError(err)
-		}
-	}()
-
-	if _, err := uuid.Parse(in.GetID()); err != nil {
-		logger.Sugar().Errorw("GetAccount", "error", err)
-		return &npool.GetAccountResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-
-	span = commontracer.TraceInvoker(span, "deposit", "deposit", "GetAccount")
-
-	info, err := deposit1.GetAccount(ctx, in.GetID())
-	if err != nil {
-		logger.Sugar().Errorw("GetAccount", "err", err)
-		return &npool.GetAccountResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	return &npool.GetAccountResponse{
-		Info: info,
-	}, nil
-}
 
 func (s *Server) GetAccounts(ctx context.Context, in *npool.GetAccountsRequest) (*npool.GetAccountsResponse, error) {
 	var err error
@@ -65,7 +34,7 @@ func (s *Server) GetAccounts(ctx context.Context, in *npool.GetAccountsRequest) 
 
 	conds := in.Conds
 	if conds == nil {
-		conds = &npool.Conds{}
+		conds = &accmgrpb.Conds{}
 	}
 
 	limit := in.GetLimit()
@@ -75,7 +44,7 @@ func (s *Server) GetAccounts(ctx context.Context, in *npool.GetAccountsRequest) 
 
 	span = commontracer.TraceInvoker(span, "deposit", "deposit", "GetAccounts")
 
-	infos, err := deposit1.GetAccounts(ctx, conds, in.GetOffset(), limit)
+	infos, total, err := accmgrcli.GetAccounts(ctx, conds, in.GetOffset(), limit)
 	if err != nil {
 		logger.Sugar().Errorw("GetAccounts", "err", err)
 		return &npool.GetAccountsResponse{}, status.Error(codes.Internal, err.Error())
@@ -83,5 +52,6 @@ func (s *Server) GetAccounts(ctx context.Context, in *npool.GetAccountsRequest) 
 
 	return &npool.GetAccountsResponse{
 		Infos: infos,
+		Total: total,
 	}, nil
 }
---
+++ pkg/client/account/client.go
@@ -0,0 +1,57 @@
+package account
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	accmgrpb "github.com/NpoolPlatform/message/npool/account/mgr/v1/account"
+	npool "github.com/NpoolPlatform/message/npool/account/mw/v1/account"
+
+	constant "github.com/NpoolPlatform/account-middleware/pkg/message/const"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
+
+func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func GetAccounts(ctx context.Context, conds *accmgrpb.Conds, offset, limit int32) ([]*accmgrpb.Account, uint32, error) {
+	total := uint32(0)
+
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetAccounts(ctx, &npool.GetAccountsRequest{
+			Conds:  conds,
+			Offset: offset,
+			Limit:  limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		total = resp.Total
+
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*accmgrpb.Account), total, nil
+}
