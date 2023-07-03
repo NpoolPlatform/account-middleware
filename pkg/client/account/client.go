c5ff4df53b97d594b216808102a57453807cb243
Zhao KK <kikakkz@hotmail.com>
2023-07-03 06:58:13 +0000

    Use right type

--- pkg/client/account/client.go
+++ pkg/client/account/client.go
@@ -6,9 +6,8 @@ import (
 
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
 
-	commonpb "github.com/NpoolPlatform/message/npool"
-	accmgrpb "github.com/NpoolPlatform/message/npool/account/mgr/v1/account"
 	npool "github.com/NpoolPlatform/message/npool/account/mw/v1/account"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 
 	servicename "github.com/NpoolPlatform/account-middleware/pkg/servicename"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
@@ -34,7 +33,7 @@ func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
 	return handler(_ctx, cli)
 }
 
-func GetAccount(ctx context.Context, id string) (*accmgrpb.Account, error) {
+func GetAccount(ctx context.Context, id string) (*npool.Account, error) {
 	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
 		resp, err := cli.GetAccount(ctx, &npool.GetAccountRequest{
 			ID: id,
@@ -47,10 +46,10 @@ func GetAccount(ctx context.Context, id string) (*accmgrpb.Account, error) {
 	if err != nil {
 		return nil, err
 	}
-	return info.(*accmgrpb.Account), nil
+	return info.(*npool.Account), nil
 }
 
-func GetAccounts(ctx context.Context, conds *accmgrpb.Conds, offset, limit int32) ([]*accmgrpb.Account, uint32, error) {
+func GetAccounts(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.Account, uint32, error) {
 	total := uint32(0)
 
 	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
@@ -70,16 +69,16 @@ func GetAccounts(ctx context.Context, conds *accmgrpb.Conds, offset, limit int32
 	if err != nil {
 		return nil, 0, err
 	}
-	return infos.([]*accmgrpb.Account), total, nil
+	return infos.([]*npool.Account), total, nil
 }
 
-func GetManyAccounts(ctx context.Context, ids []string) ([]*accmgrpb.Account, uint32, error) {
+func GetManyAccounts(ctx context.Context, ids []string) ([]*npool.Account, uint32, error) {
 	total := uint32(0)
 
 	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
 		resp, err := cli.GetAccounts(ctx, &npool.GetAccountsRequest{
-			Conds: &accmgrpb.Conds{
-				IDs: &commonpb.StringSliceVal{
+			Conds: &npool.Conds{
+				IDs: &basetypes.StringSliceVal{
 					Op:    cruder.IN,
 					Value: ids,
 				},
@@ -98,5 +97,5 @@ func GetManyAccounts(ctx context.Context, ids []string) ([]*accmgrpb.Account, ui
 	if err != nil {
 		return nil, 0, err
 	}
-	return infos.([]*accmgrpb.Account), total, nil
+	return infos.([]*npool.Account), total, nil
 }
