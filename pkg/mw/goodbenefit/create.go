124e1d20583bc2e755ef276407b407a4fa2a6cec
Zhao KK <kikakkz@hotmail.com>
2023-07-12 08:29:48 +0000

    fix lock

--- pkg/mw/goodbenefit/create.go
+++ pkg/mw/goodbenefit/create.go
@@ -97,43 +97,46 @@ func (h *Handler) CreateAccount(ctx context.Context) (*npool.Account, error) { /
 			return err
 		}
 
-		if h.Backup == nil || !*h.Backup {
-			ids, err := tx.
-				GoodBenefit.
-				Query().
-				Select().
-				Modify(func(s *sql.Selector) {
-					t := sql.Table(entaccount.Table)
-					s.LeftJoin(t).
-						On(
-							t.C(entaccount.FieldID),
-							s.C(entgoodbenefit.FieldAccountID),
-						).
-						OnP(
-							sql.EQ(t.C(entaccount.FieldCoinTypeID), *h.CoinTypeID),
-						).
-						OnP(
-							sql.EQ(t.C(entaccount.FieldDeletedAt), 0),
-						)
-				}).
-				Where(
-					entgoodbenefit.IDNEQ(*h.ID),
-				).
-				IDs(_ctx)
-			if err != nil {
-				return err
-			}
+		if h.Backup != nil && *h.Backup {
+			return nil
+		}
 
-			if _, err := tx.
-				GoodBenefit.
-				Update().
-				Where(
-					entgoodbenefit.IDIn(ids...),
-				).
-				SetBackup(true).
-				Save(_ctx); err != nil {
-				return err
-			}
+		ids, err := tx.
+			GoodBenefit.
+			Query().
+			Select().
+			Modify(func(s *sql.Selector) {
+				t := sql.Table(entaccount.Table)
+				s.LeftJoin(t).
+					On(
+						t.C(entaccount.FieldID),
+						s.C(entgoodbenefit.FieldAccountID),
+					).
+					OnP(
+						sql.EQ(t.C(entaccount.FieldCoinTypeID), *h.CoinTypeID),
+					).
+					OnP(
+						sql.EQ(t.C(entaccount.FieldDeletedAt), 0),
+					)
+			}).
+			Where(
+				entgoodbenefit.IDNEQ(*h.ID),
+				entgoodbenefit.Backup(false),
+			).
+			IDs(_ctx)
+		if err != nil {
+			return err
+		}
+
+		if _, err := tx.
+			GoodBenefit.
+			Update().
+			Where(
+				entgoodbenefit.IDIn(ids...),
+			).
+			SetBackup(true).
+			Save(_ctx); err != nil {
+			return err
 		}
 
 		return nil
--- pkg/mw/platform/create.go
+++ pkg/mw/platform/create.go
@@ -109,44 +109,47 @@ func (h *Handler) CreateAccount(ctx context.Context) (*npool.Account, error) { /
 			return err
 		}
 
-		if h.Backup == nil || !*h.Backup {
-			ids, err := tx.
-				Platform.
-				Query().
-				Select().
-				Modify(func(s *sql.Selector) {
-					t := sql.Table(entaccount.Table)
-					s.LeftJoin(t).
-						On(
-							t.C(entaccount.FieldID),
-							s.C(entplatform.FieldAccountID),
-						).
-						OnP(
-							sql.EQ(t.C(entaccount.FieldCoinTypeID), *h.CoinTypeID),
-						).
-						OnP(
-							sql.EQ(t.C(entaccount.FieldDeletedAt), 0),
-						)
-				}).
-				Where(
-					entplatform.IDNEQ(*h.ID),
-					entplatform.UsedFor(h.UsedFor.String()),
-				).
-				IDs(_ctx)
-			if err != nil {
-				return err
-			}
+		if h.Backup != nil && *h.Backup {
+			return nil
+		}
 
-			if _, err := tx.
-				Platform.
-				Update().
-				Where(
-					entplatform.IDIn(ids...),
-				).
-				SetBackup(true).
-				Save(_ctx); err != nil {
-				return err
-			}
+		ids, err := tx.
+			Platform.
+			Query().
+			Select().
+			Modify(func(s *sql.Selector) {
+				t := sql.Table(entaccount.Table)
+				s.LeftJoin(t).
+					On(
+						t.C(entaccount.FieldID),
+						s.C(entplatform.FieldAccountID),
+					).
+					OnP(
+						sql.EQ(t.C(entaccount.FieldCoinTypeID), *h.CoinTypeID),
+					).
+					OnP(
+						sql.EQ(t.C(entaccount.FieldDeletedAt), 0),
+					)
+			}).
+			Where(
+				entplatform.IDNEQ(*h.ID),
+				entplatform.UsedFor(h.UsedFor.String()),
+				entplatform.Backup(false),
+			).
+			IDs(_ctx)
+		if err != nil {
+			return err
+		}
+
+		if _, err := tx.
+			Platform.
+			Update().
+			Where(
+				entplatform.IDIn(ids...),
+			).
+			SetBackup(true).
+			Save(_ctx); err != nil {
+			return err
 		}
 
 		return nil
