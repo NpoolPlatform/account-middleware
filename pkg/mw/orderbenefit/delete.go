a2bc2c9449d12984e3234037aa18b329f50b81ab
Geapefurit <670884108@qq.com>
2024-10-08 09:06:25 +0000

    debug for ut

--- pkg/mw/orderbenefit/delete.go
+++ pkg/mw/orderbenefit/delete.go
@@ -84,7 +84,5 @@ func (h *Handler) DeleteAccountWithTx(ctx context.Context, tx *ent.Tx) error {
 }
 
 func (h *Handler) DeleteAccount(ctx context.Context) error {
-	return db.WithTx(ctx, func(ctx context.Context, tx *ent.Tx) error {
-		return nil
-	})
+	return db.WithTx(ctx, h.DeleteAccountWithTx)
 }
